60bb162a2f0b2bb5c3f7ff4fb7914a6778898e7e
Agimiriara <agimiriarascc@outlook.com>
2021-10-01 21:39:39 -0300

    LISTA1JAVA

---
+++ Java/AULAS/src/LISTA1/QUESTAO4.java
@@ -0,0 +1,29 @@
+package LISTA1;
+
+import  java.util.Scanner ;
+
+public  class  QUESTAO4{
+
+	public  static  void  main ( String [] args ) {
+		Scanner sc =  new  Scanner ( System.in);
+		int  A , B , C , R , S , D ;
+
+		System.out.println( " digite o primeiro valor: " );
+		A  = sc . nextInt ();
+		System.out.println( " digite o segundo valor: " );
+		B  = sc . nextInt ();
+		System.out.println( " digite o terceiro valor: " );
+		C  = sc . nextInt ();
+
+		R  = ( A  +  B ) * ( A  +  B );
+		S  = ( B  +  C ) * ( B  +  C );
+		D  = ( R  +  S ) /  2 ;
+
+		System.out.println("O resultado de D é :"+D);
+
+	
+		
+
+	}
+
+}
